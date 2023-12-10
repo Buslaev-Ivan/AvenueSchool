9d53b0c94327e5b3864f1688d764033d83e6fe4e
Buslaev-Ivan <mrlucky@bk.ru>
2023-12-10 22:38:27 +0300

    Знакомство с коллекциями

--- src/lesson15/Main.java
+++ src/lesson15/Main.java
@@ -1,8 +1,6 @@
 package lesson15;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,16 +14,43 @@ public class Main {
         b.add(5);
         b.add(6);
         b.add(7);
+        LinkedList<Double> c = new LinkedList<>();
+        c.add(1.2);
+        c.add(2.3);
+        c.add(0.2);
+        c.add(5.6);
+        c.add(3.4);
 
-        CollectionUtils utils = new CollectionUtilsImpl(); //разобратся
-        Collection<Integer> unionResult = utils.union(a, b); //разобратся
+        CollectionUtils collutils = new CollectionUtilsImpl(); //разобратся
+        ListUtilslmpl listutils = new ListUtilslmpl();
+        SetUtilslmpl setUtilslmpl = new SetUtilslmpl();
+        Collection<Integer> unionResult = collutils.union(a, b);
+        Collection<Integer> nums1 = new ArrayList<>();
+        nums1.add(2);
+        nums1.add(3);
+        nums1.add(4);
+        nums1.add(1);
+        nums1.add(0);
+        Set<String> nums2 = new TreeSet<>();
+        nums2.add("5");
+        nums2.add("8");
+        nums2.add("6");
+        nums2.add("9");
+        nums2.add("7");
 
         System.out.println("Коллекция а: " + a);
         System.out.println("Коллекция б: " + b);
-        System.out.println("Объединение двух коллекций с дубликатами " + unionResult);
-        System.out.println("Пересечение двух коллекций с дубликатами " + utils.intersection(a, b));
-        System.out.println("Объединение двух коллекций без дубликатов " + utils.unionWithoutDuplicate(a, b));
-        System.out.println("Пересечение двух коллекций без дубликатов" + utils.intersectionWithoutDuplicate(a, b));
-        System.out.println("Разность двух коллекций" + utils.difference(a, b));
+        System.out.println("Объединение двух коллекций а, б с дубликатами: " + unionResult);
+        System.out.println("Пересечение двух коллекций а, б с дубликатами: " + collutils.intersection(a, b));
+        System.out.println("Объединение двух коллекций а,б без дубликатов: " + collutils.unionWithoutDuplicate(a, b));
+        System.out.println("Пересечение двух коллекций а, б без дубликатов: " + collutils.intersectionWithoutDuplicate(a, b));
+        System.out.println("Разность двух коллекций а, б: " + collutils.difference(a, b));
+        System.out.println("Коллекция c: " + c);
+        System.out.println("Отсортированная по убыванию коллекция c: " + listutils.sortedList(c));
+        System.out.println("Преминение asList(111, 222, 333): " + listutils.asList("111", "222", "333"));
+        System.out.println("Преминение customOrderSet(11, 12, 33, 5, 66): " + setUtilslmpl.customOrderSet(11, 12, 33, 5, 66));
+        System.out.println("Коллекция nums1: " + nums1);
+        System.out.println("Коллекция nums2: " + nums2);
+        System.out.println("Коллекция nums1 + nums2: " + setUtilslmpl.orderedSet(nums1, nums2));
     }
 }
