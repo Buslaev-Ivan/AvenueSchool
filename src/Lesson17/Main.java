36333e6d624794529c3dcc31a6ea49a4b3032e9d
Buslaev-Ivan <mrlucky@bk.ru>
2023-12-17 19:19:16 +0300

    Освоение Map

--- src/Lesson17/Main.java
+++ src/Lesson17/Main.java
@@ -1,5 +1,42 @@
 package Lesson17;
 
 public class Main {
-    //в делении 3 catch для каждого експрессон
+    public static void main(String[] args) {
+        StringUtilsImpl testUtils = new StringUtilsImpl();
+
+//        для проверки нужно разакоментировать нужные строки
+
+//        System.out.println("метод div, входные 10,5 - результат:" + testUtils.div("10", "5"));
+//        System.out.println("метод div, входные 10,0 - результат:" + testUtils.div("10", "0"));
+//        System.out.println("метод div, входные 10,qwerty - результат:" + testUtils.div("10", "йцкуен"));
+        System.out.println();
+
+        System.out.println("метод findWord, входные -тект- Солнце светит высоко, Солнце светит далеко -слово- Солнце ");
+        int[] index = testUtils.findWord("Солнце светит высоко, Солнце светит далеко ", "Солнце");
+        for (int i = 0; i < index.length; i++) {
+            System.out.println(index[i]);
+        }
+        System.out.println();
+
+
+//        System.out.println("метод findWord, входные -тект- Солнце светит высоко, Солнце светит далеко -слово- Луна ");
+//        int[] index = testUtils.findWord("Солнце светит высоко, Солнце светит далеко ", "Луна");
+//        for (int i = 0; i < index.length; i++) {
+//            System.out.println(index[i]);
+//        }
+//        System.out.println();
+
+        System.out.println("метод findNumbers, входные: 2034.56 самолет 101, 505 свет 123.123 ");
+
+        try {  //тут не очень понятно зачем настоял на трай катч
+            testUtils.findNumbers("2034.56 самолет 101, 505 свет 123.123");
+//            testUtils.findNumbers("самолет свет");
+            double[] numbers = testUtils.findNumbers("2034.56 самолет 101, 505 свет 123.123");
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i]);
+            }
+        } catch (CustomException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/Lesson17/StringUtilsImpl.java
+++ src/Lesson17/StringUtilsImpl.java
@@ -1,5 +1,10 @@
 package Lesson17;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class StringUtilsImpl implements StringUtils {
     @Override
     public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
@@ -12,16 +17,57 @@ public class StringUtilsImpl implements StringUtils {
         if (num2 == 0) {
             throw new ArithmeticException("На ноль делить нельзя!");
         }
-        return num1/num2;
+        return num1 / num2;
     }
 
     @Override
     public int[] findWord(String text, String word) throws NullPointerException {
-        return new int[0];
+        List<String> arrayText = new ArrayList<String>(Arrays.asList(text.split("[, ]")));
+        List<Integer> arrayIndex = new ArrayList<>();
+//        System.out.println("размер " + arrayText.size());
+//        System.out.println(arrayText);
+        for (int i = 0; i < arrayText.size(); i++) {
+            if (arrayText.get(i).equalsIgnoreCase(word)) {
+                arrayIndex.add(i);
+            }
+        }
+        if (arrayIndex.size() == 0) {
+            throw new NullPointerException("Совпадений нет");
+        }
+        int index[] = new int[arrayIndex.size()];
+        for (int i = 0; i < index.length; i++) {
+            index[i] = arrayIndex.get(i);
+        }
+        return index;
     }
 
     @Override
     public double[] findNumbers(String text) throws CustomException {
-        return new double[0];
+        List<String> firstText = new ArrayList<String>(Arrays.asList(text.split("[, ]")));
+        List<String> nextText = new ArrayList<String>(Arrays.asList(text.split("[, ]")));
+        for (int i = 0; i < nextText.size(); i++) {
+            if (!isNumeric(nextText.get(i))) {
+                nextText.remove(i);
+            }
+        }
+//        System.out.println(firstText);
+//        System.out.println(nextText);
+        double[] numbers = new double[nextText.size()];
+        for (int i = 0; i < nextText.size(); i++) {
+            numbers[i] = Double.parseDouble(nextText.get(i));
+        }
+        if (firstText.size() == nextText.size()) {
+            throw new CustomException ("Числа не обнаружены");
+        }
+        return numbers;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
